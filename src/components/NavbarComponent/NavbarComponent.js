ebbe436a60f23de81cb88b103898ca848a7a24d1
hasnain-cyber <hasnainraza1072@gmail.com>
2023-12-02 01:31:04 +0530

    Navbar update

--- src/components/NavbarComponent/NavbarComponent.js
+++ src/components/NavbarComponent/NavbarComponent.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Nav, Navbar, Container, Button } from 'react-bootstrap'
 
 export default function NavbarComponent() {
     return (
-        <div className='d-flex justify-content-between'>
-            <span>BUSBUSGO</span>
-            <div className='d-flex gap-3'>
-                <Link to={'/customerLogin'}>Customer Login</Link>
-                <Link to={'/adminLogin'}>Admin Login</Link>
-            </div>
-        </div>
+        <Navbar className="bg-dark px-4 py-2">
+            <Container>
+                <Navbar.Brand as={Link} to={'/'}>
+                    <img
+                        src="/logo.png"
+                        height="30"
+                        className="d-inline-block align-top"
+                        alt="React Bootstrap logo"
+                    />
+                </Navbar.Brand>
+            </Container>
+            <Container className="justify-content-end">
+                <Button variant='info' className='ms-2'>
+                    <Nav.Link as={Link} to={'/customerLogin'} className="text-light">Customer Login</Nav.Link>
+                </Button>
+                <Button variant='info' className='ms-2'>
+                    <Nav.Link as={Link} to={'/adminLogin'} className="text-light">Admin Login</Nav.Link>
+                </Button>
+            </Container>
+        </Navbar>
     )
 }
