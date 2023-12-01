161f1cd4701b1465d95f9716493caefcf0ac0875
hasnain-cyber <hasnainraza1072@gmail.com>
2023-12-02 01:37:13 +0530

    Landing Page design

--- src/public/pages/LandingPage/LandingPage.js
+++ src/public/pages/LandingPage/LandingPage.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import NavbarComponent from '../../../components/NavbarComponent/NavbarComponent'
+import { Container } from 'react-bootstrap'
 
 export default function LandingPage() {
     return (
         <>
             <NavbarComponent />
-            <div>LandingPage</div>
+            {/* main body with only busbusgo header image in the center of the screen with size 100 vh */}
+            <Container className='d-flex justify-content-center align-items-center' style={{ height: '100vh' }}>
+                <img src='/header.png' alt='busbusgo logo' />
+            </Container>
+
         </>
     )
 }
